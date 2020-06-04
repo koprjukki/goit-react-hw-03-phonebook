f8cb0b7ccc8b2067a1e3c9af63e6ecac651c38f0
Illya Kobyliatskii <flazzygardner1234@gmail.com>
2020-06-04 17:04:30 +0300

    Updates

---
+++ precache-manifest.fab57a356b09ad353e72e44b14fb11a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db19824a862297622acbd920fd2aa03",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "aee34d982999f0fab5b7",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.3fae68eb.chunk.css"
+  },
+  {
+    "revision": "b7fa76dfc16d23555a64",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.41181ee9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.41181ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aee34d982999f0fab5b7",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.fba456c8.chunk.js"
+  },
+  {
+    "revision": "13e5c6f10c9c4eaefc70",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.a39b1c9e.js"
+  }
+]);
